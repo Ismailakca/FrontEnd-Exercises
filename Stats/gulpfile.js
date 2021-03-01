746aeca7872d2fcadf8621709686fbc24b581723
ismailakca <ismaillakkca@gmail.com>
2021-03-02 01:37:28 +0300

    -stats

---
+++ Stats/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browser = require('browser-sync').create()
+
+
+gulp.task('css',() => {
+    return gulp.src('./scss/main.scss')
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(gulp.dest('./'))
+        .pipe(browser.stream())
+})
+
+gulp.task('serve',() => {
+    browser.init({
+        server:{
+            baseDir:'./'
+        }
+    })
+    gulp.watch('./scss/**/*.scss', gulp.series(['css']))
+    gulp.watch('./*.html').on('change',browser.reload)
+})
+
+gulp.task('default',gulp.series(['serve']))
